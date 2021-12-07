07dbb3e7593198cee87c523b0eddad9727091ee6
ankuranurag2 <ankuranurag2@gmail.com>
2021-12-07 18:30:07 +0530

    Changelog: - Api call in respository

--- app/src/main/java/ankuranurag2/unsplash/data/repository/UnsplashImageRepositoryImpl.kt
+++ app/src/main/java/ankuranurag2/unsplash/data/repository/UnsplashImageRepositoryImpl.kt
@@ -1,14 +1,31 @@
 package ankuranurag2.unsplash.data.repository
 
-import ankuranurag2.unsplash.data.models.ImageDto
+import ankuranurag2.unsplash.data.models.ImageData
 import ankuranurag2.unsplash.data.remote.UnsplashApi
 import ankuranurag2.unsplash.domain.repository.UnsplashImageRepository
+import ankuranurag2.unsplash.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 class UnsplashImageRepositoryImpl @Inject constructor(
     private val api: UnsplashApi
 ) : UnsplashImageRepository {
-    override suspend fun getImages(accessKey: String, pageNum: Int): List<ImageDto> {
-        return api.getUnsplashImages(accessKey, pageNum)
+
+    override suspend fun getImages(accessKey: String, pageNum: Int)
+            : Flow<Resource<List<ImageData>>> = flow {
+
+        emit(Resource.Loading<List<ImageData>>())
+
+        try {
+            val imageList = api.getUnsplashImages(accessKey, pageNum)
+            emit(Resource.Success<List<ImageData>>(data = imageList.map { it.toImageData() }))
+        } catch (e: HttpException) {
+            emit(Resource.Error<List<ImageData>>("Oops, Something went wrong!"))
+        } catch (e: IOException) {
+            emit(Resource.Error<List<ImageData>>("Please check you internet connection!"))
+        }
     }
 }
--- app/src/main/java/ankuranurag2/unsplash/domain/repository/UnsplashImageRepository.kt
+++ app/src/main/java/ankuranurag2/unsplash/domain/repository/UnsplashImageRepository.kt
@@ -1,7 +1,9 @@
 package ankuranurag2.unsplash.domain.repository
 
-import ankuranurag2.unsplash.data.models.ImageDto
+import ankuranurag2.unsplash.data.models.ImageData
+import ankuranurag2.unsplash.utils.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface UnsplashImageRepository {
-    suspend fun getImages(accessKey: String, pageNum: Int): List<ImageDto>
+    suspend fun getImages(accessKey: String, pageNum: Int): Flow<Resource<List<ImageData>>>
 }
